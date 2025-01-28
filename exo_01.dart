4cf32c884db1e2e85074a0b7fa7fbb5dc86aef4b
Honorine684 <honorinegabiam0@gmail.com>
2025-01-28 11:53:25 +0100

    fonction aboutissant au protocole 2 beta

--- exo_01.dart
+++ exo_01.dart
@@ -27,7 +27,24 @@ print("Votre message contient :  $tailleLettre caractères");
 
 // decomposer la lettre
 print("Votre lettre décomposé donne : ${decomposerMessage(lettre)}");
+
+// test protocole beta
+
+//Inverser la lettre
+print("Votre lettre inversé donne : ");
+print(protocoleBeta(lettre));
+
+// remplacer agent par espion
+print("Le remplacement de agent par espion donne: ");
+print(remplacerAgentParEspion(lettre));
+
+// test protocole gamma
+
+// verifier que la lettre commence par cher
+//verifierQueLeMessageCommenceParCher(lettre);
 }
+
+
 
 
 int protocoleAlpha(String lettre){
@@ -38,3 +55,25 @@ List decomposerMessage(String lettre){
   List<dynamic> lettres = lettre.split(" ");
   return lettres;
 }
+
+String protocoleBeta(String lettre){
+  String lettreInverse = new String.fromCharCodes(lettre.runes.toList().reversed);
+  return lettreInverse;
+}
+
+String remplacerAgentParEspion(String lettre){
+  //List<dynamic> lettres = decomposerMessage(lettre);
+  String lettreRemplacement = lettre.replaceAll("agent", "espion");
+  return lettreRemplacement;
+}
+
+/*void verifierQueLeMessageCommenceParCher(String lettre){
+  List<dynamic> lettres = decomposerMessage(lettre);
+  for(int i=0;i<lettres.length;){
+    if(lettres[0] == "Cher"){
+      print("Votre lettre commence bien par cher");
+    }else{
+      print("Ceci ne débute pas par cher");
+    }
+  }
+}*/
