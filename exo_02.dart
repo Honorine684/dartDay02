a90d8c20709473530a7326ca7b5d1539e6201542
Honorine684 <honorinegabiam0@gmail.com>
2025-01-28 21:54:38 +0100

    protocole de gestion d'energie

--- exo_02.dart
+++ exo_02.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 void main(){
   print("Entrez une distance");
   int distance = int.parse(stdin.readLineSync()!);
@@ -7,6 +8,14 @@ void main(){
 void calculTrajet(int distance){
   identificationRoute(distance);
   print("Le nombre total de recharge pour cette distance est de : ${pointDeRecharge(distance)}");
+  print("L'estimation de la consommation energétique donne ${estimationConsommation(distance)} unité energétique");
+  num reserveEnergetique = estimationConsommation(distance);
+  if(distance > 50){
+    reserveEnergetique = reserveEnergetique*0.8 ;
+  }else{
+     reserveEnergetique;
+  }
+
 }
 
 void identificationRoute(int distance){
@@ -20,3 +29,11 @@ void identificationRoute(int distance){
 num pointDeRecharge(int distance){
   return distance/10;
 }
+num estimationConsommation(int distance){
+  return pow(distance, 2);
+}
+
+/*num applicationBoost(int distance){
+
+
+}*/
